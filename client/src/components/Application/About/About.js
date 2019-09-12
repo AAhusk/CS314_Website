c1abe106b7fed360438f859f0df09d550d2eac62
Sean Boyd <seanboyd10@gmail.com>
2019-09-12 13:39:14 -0600

    Format About.js cards

--- client/src/components/Application/About/About.js
+++ client/src/components/Application/About/About.js
@@ -14,45 +14,56 @@ export default class About extends Component{
     render() {
         return (
             //<h1>About Page</h1>
-
             <Container>
-                <Row xs={12} sm={6} md={4} lg={3}>
-                    <Card>
-                        <CardImg top width="25%" src={AaronImg} alt="Aaron Huskerson img" />
-                        <CardBody>
-                            <CardTitle>Aaron Huskerson</CardTitle>
-                            <CardText>I'm a junior majoring in Computer Science, and getting a minor in Music. I like to play piano, guitar, smash bros, and explore natural areas. </CardText>
-                        </CardBody>
-                    </Card>
+                
+                <Row>
+                    <Col xs={12} sm={6} md={4} lg={3}>
+                        <Card>
+                            <CardImg top width="25%" src={AaronImg} alt="Aaron Huskerson img" />
+                            <CardBody>
+                                <CardTitle>Aaron Huskerson</CardTitle>
+                                <CardText>I'm a junior majoring in Computer Science, and getting a minor in Music. I like to play piano, guitar, smash bros, and explore natural areas. </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
 
-                    <Card>
-                        <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
-                        <CardBody>
-                            <CardTitle>Calvin Davis</CardTitle>
-                            <CardText>Third Year CS student, Russian minor, AFROTC.</CardText>
-                        </CardBody>
-                    </Card>
+                    <Col xs={12} sm={6} md={4} lg={3}>
+                        <Card>
+                            <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
+                            <CardBody>
+                                <CardTitle>Calvin Davis</CardTitle>
+                                <CardText>Third Year CS student, Russian minor, AFROTC.</CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
 
-                    <Card>
-                        <CardImg top width="25%" src={ChanImg}  alt="Chandler Day img" />
-                        <CardBody>
-                            <CardTitle>Chandler Day</CardTitle>
-                            <CardText>I am a Third Year Computer Science major with a minor in Mathematics. I intend on graduating in May of 2020. I enjoy science fiction and fantasy, listening to music, and playing video games.</CardText>
-                        </CardBody>
-                    </Card>
+                    <Col xs={12} sm={6} md={4} lg={3}>
+                        <Card>
+                            <CardImg top width="25%" src={ChanImg}  alt="Chandler Day img" />
+                            <CardBody>
+                                <CardTitle>Chandler Day</CardTitle>
+                                <CardText>I am a Third Year Computer Science major with a minor in Mathematics. I intend on graduating in May of 2020. I enjoy science fiction and fantasy, listening to music, and playing video games.</CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
 
-                    <Card>
-                        <CardImg top width="25%" src={SeanImg}  alt="Sean Boyd Image" />
-                        <CardBody>
-                            <CardTitle>Sean Boyd</CardTitle>
-                            <CardText> Sean Boyd is pursuing a degree in computer engineering at Colorado State
-                                       University. On the side, Sean is devloping a mobile app as an intern for Gerastay,
-                                       a startup company. Sean enjoys snowboarding, online gaming, and doing hands on
-                                       projects in his free time.
-                            </CardText>
-                        </CardBody>
-                    </Card>
+                    <Col xs={12} sm={6} md={4} lg={3}>
+                        <Card>
+                            <CardImg top width="25%" src={SeanImg}  alt="Sean Boyd Image" />
+                            <CardBody>
+                                <CardTitle>Sean Boyd</CardTitle>
+                                <CardText> Sean Boyd is pursuing a degree in computer engineering at Colorado State
+                                           University. On the side, Sean is devloping a mobile app as an intern for Gerastay,
+                                           a startup company. Sean enjoys snowboarding, online gaming, and doing hands on
+                                           projects in his free time.
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+
                 </Row>
+             
+
             </Container>
         )
     }
