68df2940cb25f6dfda7e81deea4caccfb88651cf
cdavis10 <Sv153809>
2019-09-11 20:51:02 -0600

    Added a basic skeleton for all the cards in the about page. Not super pretty but gets the job done, also works properly with mobile.

--- client/src/components/Application/About/About.js
+++ client/src/components/Application/About/About.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle, Container, Row} from 'reactstrap';
+
+import CalImg from './images/CalvinDavis.jpg'; // I had to scale it down to 240x240 manually
+
+
 
 export default class About extends Component{
     constructor(props) {
@@ -6,8 +11,63 @@ export default class About extends Component{
     }
 
     render() {
-        return(
-            <h1>About Page</h1>
+        return (
+            //<h1>About Page</h1>
+
+            <Container>
+                <Row>
+                    <Card>
+                        <CardImg top width="25%" src={CalImg} alt="Calvin Davis img" />
+                        <CardBody>
+                            <CardTitle>Calvin Davis</CardTitle>
+                            <CardText>Third Year CS student ready to rock and roll.</CardText>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
+                        <CardBody>
+                            <CardTitle>Calvin Davis</CardTitle>
+                            <CardText>Third Year CS student.</CardText>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
+                        <CardBody>
+                            <CardTitle>Calvin Davis</CardTitle>
+                            <CardText>Third Year CS student.</CardText>
+                        </CardBody>
+                    </Card>
+
+                    <Card>
+                        <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
+                        <CardBody>
+                            <CardTitle>Calvin Davis</CardTitle>
+                            <CardText>Third Year CS student.</CardText>
+                        </CardBody>
+                    </Card>
+                </Row>
+            </Container>
         )
     }
 }
+
+
+// const Example = (props) => {
+//     render() {
+//         return (
+//             <div>
+//                 <Card>
+//                     <CardImg top width="25%" src="./images/CalvinDavis.jpg" alt="Calvin Davis img" />
+//                     <CardBody>
+//                         <CardTitle>Calvin Davis</CardTitle>
+//                         <CardText>Third year CS major ready to rock and roll.</CardText>
+//                     </CardBody>
+//                 </Card>
+//             </div>
+//         );
+//     }
+// };
+//
+// export default Example;
