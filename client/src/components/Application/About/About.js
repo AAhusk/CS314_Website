9e180a753eda858b72affbb06036687974b6e48c
MyNameIsCalvinDavis <mynameiscalvindavis@gmail.com>
2019-10-07 15:44:01 -0600

    Optimize About.js, Calc.js, App.js

--- client/src/components/Application/About/About.js
+++ client/src/components/Application/About/About.js
@@ -11,57 +11,37 @@ export default class About extends Component{
         super(props);
     }
 
-    //
+    renderCard(src, alt, title, text) {
+        return (
+            <Col xs={12} sm={6} md={4} lg={3}>
+                <Card>
+                    <CardImg top width="25%" src={src} alt="alt" />
+                    <CardBody>
+                        <CardTitle>{title}</CardTitle>
+                        <CardText>{text}</CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
 
     render() {
         return (
-            //<h1>About Page</h1>
             <Container>
-                
                 <Row>
-                    <Col xs={12} sm={6} md={4} lg={3}>
-                        <Card>
-                            <CardImg top width="25%" src={AaronImg} alt="Aaron Huskerson img" />
-                            <CardBody>
-                                <CardTitle>Aaron Huskerson</CardTitle>
-                                <CardText>I'm a junior majoring in Computer Science, and getting a minor in Music. I like to play piano, guitar, smash bros, and explore natural areas. </CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-
-                    <Col xs={12} sm={6} md={4} lg={3}>
-                        <Card>
-                            <CardImg top width="25%" src={CalImg}  alt="Calvin Davis img" />
-                            <CardBody>
-                                <CardTitle>Calvin Davis</CardTitle>
-                                <CardText>Third Year CS student, Russian minor, AFROTC.</CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-
-                    <Col xs={12} sm={6} md={4} lg={3}>
-                        <Card>
-                            <CardImg top width="25%" src={ChanImg}  alt="Chandler Day img" />
-                            <CardBody>
-                                <CardTitle>Chandler Day</CardTitle>
-                                <CardText>I am a Third Year Computer Science major with a minor in Mathematics. I intend on graduating in May of 2020. I enjoy science fiction and fantasy, listening to music, and playing video games.</CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-
-                    <Col xs={12} sm={6} md={4} lg={3}>
-                        <Card>
-                            <CardImg top width="25%" src={SeanImg}  alt="Sean Boyd Image" />
-                            <CardBody>
-                                <CardTitle>Sean Boyd</CardTitle>
-                                <CardText> Sean Boyd is pursuing a degree in computer engineering at Colorado State
-                                           University. On the side, Sean is devloping a mobile app as an intern for Gerastay,
-                                           a startup company. Sean enjoys snowboarding, online gaming, and doing hands on
-                                           projects in his free time. </CardText>
-                            </CardBody>
-                        </Card>
-                    </Col>
-
+                    {this.renderCard(AaronImg, "Aaron Huskerson img", "Aaron Huskerson",
+                        "I'm a junior majoring in Computer Science, and getting a minor in Music. " +
+                        "I like to play piano, guitar, smash bros, and explore natural areas. ")}
+                    {this.renderCard(CalImg, "Calvin Davis img", "Calvin Davis",
+                        "Third Year CS student, Russian minor, AFROTC.")}
+                    {this.renderCard(ChanImg, "Chandler Day img", "Chandler Day",
+                    "I am a Third Year Computer Science major with a minor in Mathematics. I intend on graduating " +
+                        "in May of 2020. I enjoy science fiction and fantasy, listening to music, and playing video games.")}
+                    {this.renderCard(SeanImg, "Sean Boyd img", "Sean Boyd",
+                    "Sean Boyd is pursuing a degree in computer engineering at Colorado State " +
+                        "University. On the side, Sean is devloping a mobile app as an intern for Gerastay, " +
+                        "a startup company. Sean enjoys snowboarding, online gaming, and doing hands on " +
+                        "projects in his free time.")}
                 </Row>
              
 
@@ -69,22 +49,3 @@ export default class About extends Component{
         )
     }
 }
-
-
-// const Example = (props) => {
-//     render() {
-//         return (
-//             <div>
-//                 <Card>
-//                     <CardImg top width="25%" src="./images/CalvinDavis.jpg" alt="Calvin Davis img" />
-//                     <CardBody>
-//                         <CardTitle>Calvin Davis</CardTitle>
-//                         <CardText>Third year CS major ready to rock and roll.</CardText>
-//                     </CardBody>
-//                 </Card>
-//             </div>
-//         );
-//     }
-// };
-//
-// export default Example;
