9e0e8ee3c7607352632772ff910bb7c59a0f433f
MyNameIsCalvinDavis <mynameiscalvindavis@gmail.com>
2019-10-15 15:08:30 -0600

    Tested and it works, except for a bug
    Bug randomly deletes points after pressing the button, working on a fix

--- client/src/components/Application/Itinerary/Itinerary.js
+++ client/src/components/Application/Itinerary/Itinerary.js
@@ -11,6 +11,7 @@ export default class Itinerary extends Component {
     
     this.onFileSelect = this.onFileSelect.bind(this);
     this.errorHandler = this.errorHandler.bind(this);
+    this.updateData = this.updateData.bind(this);
 
     this.state = {
       trip: null,
@@ -25,7 +26,6 @@ export default class Itinerary extends Component {
   render() {
     return (
         <Container>
-          {/* {this.state.errorMessage} */}
           <LMap itineraryData={this.state.itineraryData}/>
           <Card>
             <CardHeader>Itinerary</CardHeader>   
@@ -36,10 +36,16 @@ export default class Itinerary extends Component {
           </Card>
           <Card>
             <ItineraryTable itineraryData={this.state.itineraryData}
-                            totalDistance={this.state.totalDistance}/>           
+                            totalDistance={this.state.totalDistance}
+                            updateData={this.updateData}/>
           </Card>
         </Container>
     );
+  }
+
+  updateData(state, prop){
+      console.log("UpdateData " + state + ": " + prop);
+      this.setState({[state]: prop})
   }
 
   onFileSelect(trip, itineraryData, totalDistance){
--- client/src/components/Application/Itinerary/ItineraryTable.js
+++ client/src/components/Application/Itinerary/ItineraryTable.js
@@ -3,82 +3,83 @@ import {Button, Table} from 'reactstrap';
 import {sendServerRequestWithBody} from "../../../api/restfulAPI";
 
 export default class ItineraryTable extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    // this.renderTripItinerary = this.renderTripItinerary.bind(this);
-    // this.formatTripData = this.formatTripData.bind(this);
+        this.myServerRequest = this.myServerRequest.bind(this);
+        this.insertPlacesIntoItineraryData = this.insertPlacesIntoItineraryData.bind(this);
+        this.extractPlacesFromItineraryData = this.extractPlacesFromItineraryData.bind(this);
 
-    this.state = {
-      itineraryData: null,
-      totalDistance: null
-    };
-  }
-  
-
-
-  render(){
-    console.log('ItineraryTable: ', this.props, this.state);
-
-    if (this.props.itineraryData == null){
-      return(null);
+        this.state = {
+            itineraryData: this.props.itineraryData,
+            totalDistance: null
+        };
     }
-  
-    else{
-      return (
 
-        <Table striped>
-            <Button color='primary' onClick={() => this.myServerRequest()}>Shorten Trip</Button>
+    render() {
+        //console.log('ItineraryTable: ', this.props, this.state);
 
-            <thead>
-            <tr>
-              <th>Origin</th>
-              <th>Destination</th>
-              <th>Distance</th>
-            </tr>
-          </thead>
-          
-          <tbody>
-            {this.props.itineraryData.map(this.renderTripItinerary)}
-          </tbody>
+        if (this.props.itineraryData == null) {
+            return (null);
+        } else {
+            return (
 
-          <tbody>
-            <tr>
-              <th></th>
-              <th>Total Distance</th>
-              <th>{this.props.totalDistance}</th>
-            </tr>
-          </tbody>   
-        </Table>
-      );
+                <Table striped>
+                    <Button color='primary' onClick={() => this.myServerRequest()}>Shorten Trip</Button>
+
+                    <thead>
+                    <tr>
+                        <th>Origin</th>
+                        <th>Destination</th>
+                        <th>Distance</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    {this.props.itineraryData.map(this.renderTripItinerary)}
+                    </tbody>
+
+                    <tbody>
+                    <tr>
+                        <th></th>
+                        <th>Total Distance</th>
+                        <th>{this.props.totalDistance}</th>
+                    </tr>
+                    </tbody>
+                </Table>
+            );
+        }
     }
-  }
 
     myServerRequest() {
         let tipObject = {
-            "requestType"    : "trip",
-            "requestVersion" : 2,
-            "options"        : {    //  Required in request & response
+            "requestType": "trip",
+            "requestVersion": 2,
+            "options": {    //  Required in request & response
                 "title": "Short Trip",
                 "earthRadius": "3975", // Required
                 "optimization": "short"
             },
-            "places": [  //  Required in request & response
-                {"name":"Denveer",      "latitude": "49.7", "longitude": "-115.0"},
-                {"name":"Boulder",      "latitude": "40.0", "longitude": "-105.4"},
-                {"name":"Boulder",      "latitude": "40.0", "longitude": "-105.4"},
-                {"name":"Fort Collins", "latitude": "40.6", "longitude": "-105.1"}],
-            "distances"      : []   //  Required in response
+            "places": this.extractPlacesFromItineraryData(),
+            // "places": [  //  Required in request & response
+            //     {"name": "Denveer", "latitude": "49.7", "longitude": "-115.0"},
+            //     {"name": "Boulder", "latitude": "40.0", "longitude": "-105.4"},
+            //     {"name": "Boulder", "latitude": "40.0", "longitude": "-105.4"},
+            //     {"name": "Fort Collins", "latitude": "40.6", "longitude": "-105.1"}],
+            // "distances": []   //  Required in response
         };
 
 
         sendServerRequestWithBody('trip', tipObject, this.props.serverPort)
             .then((response) => {
                 if (response.statusCode >= 200 && response.statusCode <= 299) {
+                    console.log(response.body);
                     this.setState({
                         places: response.body.places,
                         errorMessage: null
-                    }, () => {console.log(this.state.places)});
+                    }, () => {
+                        this.insertPlacesIntoItineraryData()
+                    });
                 } else {
 
                     console.log(response.statusCode + response.statusText);
@@ -93,13 +94,35 @@ export default class ItineraryTable extends React.Component {
             });
     }
 
-  renderTripItinerary(entry, index){
-    return(
-          <tr key={index}>
-            <td>{entry.origin.name}</td>
-            <td>{entry.destination.name}</td>
-            <td>{entry.distance}</td>
-          </tr>
-    );
-  }
+    extractPlacesFromItineraryData() {
+        let places = [];
+        for (let i = 0; i < this.props.itineraryData.length; i++) {
+            places.push(this.props.itineraryData[i].origin);
+        }
+        return places;
+    }
+
+    insertPlacesIntoItineraryData() {
+        let places = this.state.places;
+        let ItinData = this.props.itineraryData;
+        for (let i = 0; i < ItinData.length - 1; i++) {
+            ItinData[i].origin = places[i];
+            ItinData[i].destination = places[i+1]
+        }
+        //ItinData[this.props.itineraryData.length - 1].origin = places[places.length - 1]
+        ItinData[this.props.itineraryData.length - 1].destination = places[0];
+
+        this.setState({itineraryData: ItinData}); // Local data
+        this.props.updateData("itineraryData", ItinData);
+    }
+
+    renderTripItinerary(entry, index) {
+        return (
+            <tr key={index}>
+                <td>{entry.origin.name}</td>
+                <td>{entry.destination.name}</td>
+                <td>{entry.distance}</td>
+            </tr>
+        );
+    }
 }
