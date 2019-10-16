059db299c2d57c26ec9aa1bce7deedb04f37115c
MyNameIsCalvinDavis <mynameiscalvindavis@gmail.com>
2019-10-16 10:23:42 -0600

    Fix LMap not working
    There was an issue with how I was trying to remove errors from LMap - I've removed these changes and maps render now

--- client/src/components/Application/LMap.js
+++ client/src/components/Application/LMap.js
@@ -8,7 +8,9 @@ import Pane from './Pane';
 import iconblue from './images/iconblue.png';
 import iconblueD from './images/iconblueD.png';
 import iconred from './images/iconred.png';
+//import icongreen from './images/icongreen.png'
 import iconhappy from './images/iconhappy.png'
+//import iconflower from './images/iconflower.png'
 
 export default class LMap extends Component {
   constructor(props) {
@@ -39,7 +41,7 @@ export default class LMap extends Component {
     for (let i = 0; i < pointArr.length; i++) {
       MarkerArr.push(
           <Marker key={"Marker"+i}
-                  position={L.LatLng(pointArr[i].origin.latitude, pointArr[i].origin.longitude)}
+                  position={L.latLng(pointArr[i].origin.latitude, pointArr[i].origin.longitude)}
                   icon={this.markerIcon(iconred)}>
             <Popup className="font-weight-extrabold">
               Destination:<br/>
@@ -104,7 +106,7 @@ export default class LMap extends Component {
     if (this.props.currentLocation != null) {
       currentLocationMarker = (
           <Marker key={"CurrentLocationMarker"}
-                  position={L.LatLng(this.props.currentLocation.latitude, this.props.currentLocation.longitude)}
+                  position={L.latLng(this.props.currentLocation.latitude, this.props.currentLocation.longitude)}
                   icon={this.markerIcon(iconhappy)}>
             <Popup className="font-weight-extrabold">
               Here I am!<br/>
@@ -139,21 +141,21 @@ export default class LMap extends Component {
   }
 
   currentLocation() {
-    return L.LatLng(this.props.locationOrigin.latitude, this.props.locationOrigin.longitude);
+    return L.latLng(this.props.locationOrigin.latitude, this.props.locationOrigin.longitude);
   }
 
   currentDestination() {
-    return L.LatLng(this.props.locationDestination.latitude, this.props.locationDestination.longitude);
+    return L.latLng(this.props.locationDestination.latitude, this.props.locationDestination.longitude);
   }
 
   coloradoGeographicBoundaries() {
     // northwest and southeast corners of the state of Colorado
-    return L.LatLngBounds(L.LatLng(41, -109), L.LatLng(37, -102));
+    return L.latLngBounds(L.latLng(41, -109), L.latLng(37, -102));
 
   }
 
   csuOvalGeographicCoordinates() {
-    return L.LatLng(40.576179, -105.080773);
+    return L.latLng(40.576179, -105.080773);
   }
 
   markerIcon(url=icon) {
--- server/src/main/java/com/tripco/t11/TIP/TIPConfig.java
+++ server/src/main/java/com/tripco/t11/TIP/TIPConfig.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 /** This class defines the Config response that provides the client
  * with server specific configuration information.
- *  
+ *
  * When used with restful API services,
  * An object is created from the request JSON by the MicroServer using GSON.
  * The buildResponse method is called to set the configuration information.
  * The MicroServer constructs the response JSON from the object using GSON.
- *  
+ *
  * When used for testing purposes,
  * An object is created using the constructor below.
  * The buildResponse method is called to set the configuration information.
