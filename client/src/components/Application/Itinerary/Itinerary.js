938ab8323bb1470f0b315d049c72a6125f443c4b
aahusk <54596652+aahusk@users.noreply.github.com>
2019-10-16 20:18:25 -0600

    Added exportJSON and exportCSV buttons

--- client/src/components/Application/Itinerary/Itinerary.js
+++ client/src/components/Application/Itinerary/Itinerary.js
@@ -12,6 +12,8 @@ export default class Itinerary extends Component {
     
     this.onFileSelect = this.onFileSelect.bind(this);
     this.errorHandler = this.errorHandler.bind(this);
+    this.createOutputJSON = this.createOutputJSON.bind(this);
+    this.createOutputCSV = this.createOutputCSV.bind(this);
 
     this.state = {
       trip: null,
@@ -41,20 +43,73 @@ export default class Itinerary extends Component {
           </Card>
           <Card>
             <Row>
-              <Button className='bg-csu-gold text-white' onClick={this.createOutputJSON}>Export JSON</Button>
-              <Button className='bg-csu-gold text-white' onClick={this.createOutputCSV}>Export CSV</Button>
+              <Button href="#" id="TripJSON" className='bg-csu-gold text-white' onClick={() => this.createOutputJSON()}>Export JSON</Button>
+              <Button href="#" id="TripCSV" className='bg-csu-gold text-white' onClick={() => this.createOutputCSV()}>Export CSV</Button>
             </Row>
           </Card>
         </Container>
     );
   }
-  createOutputJSON() {
-    var TIPTrip = [];
 
+
+  createOutputJSON() {
+     if (this.state.trip == null) {
+       this.errorHandler("No file to export", 201);
+     }
+     else {
+
+       let TIPTrip = {
+         "requestType"    : "trip",
+         "requestVersion" : 2,
+         "options"        : this.state.trip.options,
+         "places"         : [],
+         "distances"      : []
+       }
+
+       let quantityPlaces = this.state.itineraryData.length;
+       for (let i=0; i<quantityPlaces; ++i) {
+         TIPTrip.places[i] = this.state.itineraryData[i].origin.name;
+         TIPTrip.distances[i] = this.state.itineraryData[i].distance;
+       }
+
+       let a = document.getElementById("TripJSON");
+       let file = new Blob([JSON.stringify(TIPTrip, null, 2)],
+           {type: 'application/json'});
+       a.href = URL.createObjectURL(file);
+       a.download = 'TIPTrip.json';
+     }
   }
 
   createOutputCSV() {
+    if (this.state.itineraryData == null) {
+      this.errorHandler("No file to export", 202)
+    }
 
+    else {
+      const TripArray = [[]];
+      TripArray[0] = ["Origin", "Destination", "Distance"];
+
+
+      for (var i = 0; i < this.state.itineraryData.length; ++i) {
+        let TripSegment = [this.state.itineraryData[i].origin.name,
+          this.state.itineraryData[i].destination.name,
+          this.state.itineraryData[i].distance];
+        TripArray[i+1] = TripSegment;
+      }
+
+      let TripCSV = "";
+
+      TripArray.forEach(function (rowArray) {
+        let row = rowArray.join(",");
+        TripCSV += row + "\r\n";
+      });
+
+      let downloadCSV = document.getElementById("TripCSV");
+      let file = new Blob([TripCSV], {type: 'text/csv'},
+          {type: 'application/json'});
+      downloadCSV.href = URL.createObjectURL(file);
+      downloadCSV.download = 'TIPTrip.csv';
+    }
   }
 
   onFileSelect(trip, itineraryData, totalDistance){
