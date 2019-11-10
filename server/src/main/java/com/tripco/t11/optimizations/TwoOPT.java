912eaeb7b1a4248e3ccce665cd50ea995514b5ec
aahusk <54596652+aahusk@users.noreply.github.com>
2019-11-09 17:20:36 -0700

    Optimize 2-opt by using a two-dimensional array to hold the distances between every place. Created methods in TIPTrip for calculations shared between nearest neighbor and 2-opt.

--- server/src/main/java/com/tripco/t11/optimizations/TwoOPT.java
+++ server/src/main/java/com/tripco/t11/optimizations/TwoOPT.java
@@ -1,43 +1,31 @@
 package com.tripco.t11.optimizations;
 
-import com.tripco.t11.misc.GreatCircleDistance;
-
-
-import java.util.List;
-import java.util.Map;
-
-
 public class TwoOPT {
 
-
-  private List<Map> shorterRoute;
-  private List<Integer> distances;
-  private double earthRadius;
-
-  private long distOneToTwo;
-  private long distThreeToFour;
-  private long distOneToThree;
-  private long distTwoToFour;
+  private int[][] distTable;
+  private int[] shorterRoute;
 
 
-  public TwoOPT(List<Map> places, List<Integer> distances, double earthRadius) {
-    this.shorterRoute = places;
-    this.distances = distances;
-    this.earthRadius = earthRadius;
+  public TwoOPT(int[] nearestRoute, int[][] distTable) {
+    createRouteArray(nearestRoute);
+    this.distTable = distTable;
   }
 
-  public List<Map> shorterRoute() {
-    Map start = shorterRoute.get(0);
-    shorterRoute.add(start);
-    int lastPlaceIndex = shorterRoute.size() - 1;
+  public int[] shorterRoute() {
+    int lastPlaceIndex = this.shorterRoute.length-1;
     this.findImprovements(lastPlaceIndex);
-    shorterRoute.remove(lastPlaceIndex);
     return this.shorterRoute;
   }
 
-  /*public List<Integer> getDistances() {
-    return this.distances;
-  }*/
+  private void createRouteArray(int[] nearestRoute) {
+    this.shorterRoute = new int[nearestRoute.length+1];
+    for (int i=0; i<nearestRoute.length; ++i) {
+      this.shorterRoute[i] = nearestRoute[i];
+    }
+    this.shorterRoute[this.shorterRoute.length-1] = nearestRoute[0];
+
+  }
+
 
   private void findImprovements(int lastPlaceIndex){
     boolean improvement = true;
@@ -45,7 +33,10 @@ public class TwoOPT {
       improvement = false;
       for (int i = 0; i <= lastPlaceIndex-3; ++i) {
         for (int k = i+2; k <= lastPlaceIndex-1; ++k) {
-          this.fourDistances(i, k);
+          long distOneToTwo = this.distTable[shorterRoute[i]][shorterRoute[i+1]];
+          long distThreeToFour = this.distTable[shorterRoute[k]][shorterRoute[k+1]];
+          long distOneToThree = this.distTable[shorterRoute[i]][shorterRoute[k]];
+          long distTwoToFour = this.distTable[shorterRoute[i+1]][shorterRoute[k+1]];
           long delta = (-distOneToTwo - distThreeToFour) + (distOneToThree + distTwoToFour);
           if(delta < 0) {
             reverse(i+1, k);
@@ -58,23 +49,11 @@ public class TwoOPT {
 
   private void reverse(int i1, int k) {
     while (i1<k) {
-      Map placeI = this.shorterRoute.get(i1);
-      this.shorterRoute.set(i1, this.shorterRoute.get(k));
-      this.shorterRoute.set(k, placeI);
+      int placeI = this.shorterRoute[i1];
+      this.shorterRoute[i1] = this.shorterRoute[k];
+      this.shorterRoute[k] = placeI;
       ++i1; --k;
     }
   }
 
-  private void fourDistances(int i, int k) {
-    GreatCircleDistance oneToTwo =
-        new GreatCircleDistance(shorterRoute.get(i), shorterRoute.get(i+1), this.earthRadius);
-    GreatCircleDistance threeToFour =
-        new GreatCircleDistance(shorterRoute.get(k), shorterRoute.get(k+1), this.earthRadius);
-    GreatCircleDistance oneToThree =
-        new GreatCircleDistance(shorterRoute.get(i), shorterRoute.get(k), this.earthRadius);
-    GreatCircleDistance twoToFour =
-        new GreatCircleDistance(shorterRoute.get(i+1), shorterRoute.get(k+1), this.earthRadius);
-    this.distOneToTwo = oneToTwo.CalculateDistance(); this.distThreeToFour = threeToFour.CalculateDistance();
-    this.distOneToThree = oneToThree.CalculateDistance(); this.distTwoToFour = twoToFour.CalculateDistance();
-  }
 }
