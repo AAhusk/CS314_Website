52e7128732307720bbca41085a5b4c0fbc4966c9
aahusk <54596652+aahusk@users.noreply.github.com>
2019-11-20 10:49:06 -0700

    Add new card to render custom units

--- client/src/components/Application/Options/Units.js
+++ client/src/components/Application/Options/Units.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Card, CardHeader, CardBody } from 'reactstrap'
+import { Card, CardHeader, CardBody, CardText } from 'reactstrap'
 import { Button, ButtonGroup } from 'reactstrap'
+import {Row, Col} from 'reactstrap'
 
 export default class Units extends Component {
   constructor(props) {
@@ -14,14 +15,38 @@ export default class Units extends Component {
 
   render() {
     return(
-      <Card className='text-left'>
-          <CardHeader className='bg-csu-green text-white font-weight-semibold'>Units</CardHeader>
-          <CardBody>
-              <ButtonGroup horizontal={"true"} className='w100'>
-                {this.renderUnitButtons(Object.keys(this.props.options.units))}
-              </ButtonGroup>
-          </CardBody>
-      </Card>
+        <CardGroup>
+          <Card className='text-left'>
+            <CardHeader className='bg-csu-green text-white font-weight-semibold'>Units</CardHeader>
+            <CardBody>
+              <Col xs={12} sm={12} md={9} lg={9}>
+                <ButtonGroup className="mr-2">
+                  {this.renderUnitButtons(Object.keys(this.props.options.units))}
+                </ButtonGroup>
+              </Col>
+            </CardBody>
+          </Card>
+
+          <Card className='text-left'>
+            <CardHeader className='bg-csu-green text-white font-weight-semibold'>Create Custom Unit</CardHeader>
+            <CardBody>
+              <CardText> Enter the name of the unit you would like to create. Then enter the value of the Earth's radius in terms of this unit. </CardText>
+              <Row>
+                <Col xs={12} sm={12} md={9} lg={7} xl={6}>
+                  {this.renderCustomUnit('customUnitName')}
+                </Col>
+                <Col xs={12} sm={12} md={9} lg={7} xl={6}>
+                  {this.renderCustomUnit('customUnitRadius')}
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} sm={12} md={9} lg={7} xl={6}>
+                  {this.renderEnterButton()}
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </CardGroup>
     );
   }
 
@@ -37,6 +62,14 @@ export default class Units extends Component {
         {unit.charAt(0).toUpperCase() + unit.slice(1)}
       </Button>
     );
+  }
+
+  renderCustomUnit() {
+
+  }
+
+  renderEnterButton() {
+
   }
 
 }
