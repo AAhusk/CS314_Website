990defd2232a14231fdebd0b09404418cdd33dc4
aahusk <54596652+aahusk@users.noreply.github.com>
2019-12-06 13:48:15 -0700

    Adjust SQL syntax and make filter fixes

--- server/src/main/java/com/tripco/t11/TIP/TIPLocation.java
+++ server/src/main/java/com/tripco/t11/TIP/TIPLocation.java
@@ -57,7 +57,7 @@ public class TIPLocation extends TIPHeader {
       pass = null;
     }
     else if(isDevelopment != null && isDevelopment.equals("development")) {
-      myUrl = "jdbc:mysql://faure.cs.colostate.edu/cs314";
+      myUrl = "jdbc:mysql://127.0.0.1:56247/cs314";
       user = "cs314-db";
       pass = "eiK5liet1uej";
     }
@@ -78,63 +78,61 @@ public class TIPLocation extends TIPHeader {
                      "INNER JOIN country on continent.id = country.continent " +
                      "INNER JOIN region on country.id = region.iso_country " +
                      "INNER JOIN world on region.id = world.iso_region " +
-                     "WHERE country.name LIKE " + m +
+                     "WHERE (country.name LIKE " + m +
                      " OR region.name LIKE " + m +
                      " OR world.name LIKE " + m +
                      " OR world.municipality LIKE " + m +
                      " OR continent.name LIKE " + m +
-                     " OR world.id LIKE " + m;
+                     " OR world.id LIKE " + m + ")";
+        for (Map.Entry<String, List<String>> entry : narrow.entrySet()) {
+          String key = entry.getKey();
+          List<String> values = entry.getValue();
 
-      for(Map.Entry<String, List<String>> entry : narrow.entrySet()) {
-        String key = entry.getKey();
-        List<String> values = entry.getValue();
-
-        switch(key.toString()) {
+          switch (key) {
             case "type":
-                for(String val : values) {
-                    query += " AND type.name LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND type.name LIKE '%" + val + "%'";
+              }
+              break;
             case "country":
-                for(String val : values) {
-                    query += " AND country.name LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND country.name LIKE '%" + val + "%'";
+              }
+              break;
             case "region":
-                for(String val : values) {
-                    query += " AND region.name LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND region.name LIKE '%" + val + "%'";
+              }
+              break;
             case "municipality":
-                for(String val : values) {
-                    query += " AND world.municipality LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND world.municipality LIKE '%" + val + "%'";
+              }
+              break;
             case "name":
-                for(String val : values) {
-                    query += " AND world.name LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND world.name LIKE '%" + val + "%'";
+              }
+              break;
             case "continent":
-                for(String val : values) {
-                    query += " AND continent.name LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND continent.name LIKE '%" + val + "%'";
+              }
+              break;
             case "id":
-                for(String val : values) {
-                    query += " AND world.id LIKE '%" + m + "%'";
-                }
-                break;
+              for (String val : values) {
+                query += " AND world.id LIKE '%" + val + "%'";
+              }
+              break;
             default:
-                break;
+              break;
+          }
         }
-        query += " ORDER BY continent.name, country.name, region.name, world.municipality, world.name ASC";
-      }
-      
+      query += " ORDER BY continent.name, country.name, region.name, world.municipality, world.name ASC";
       /*if(this.limit != 0) {
           query += " LIMIT " + this.limit;
       }*/
-                     
+      log.info("THIS IS THE QUERY: " + query);
       try (Connection conn = DriverManager.getConnection(myUrl, user, pass);
            Statement stCount = conn.createStatement();
            Statement stQuery = conn.createStatement();
@@ -149,7 +147,7 @@ public class TIPLocation extends TIPHeader {
                       rsQuery.getString("world.longitude"), rsQuery.getString("world.id"),
                       rsQuery.getString("world.municipality"), rsQuery.getString("world.altitude"),
                       rsQuery.getString("region.name"), rsQuery.getString("country.name"),
-                      rsQuery.getString("continent.name"), rsQuery.getString("type.name"));
+                      rsQuery.getString("continent.name"));
               places.add(p);
           }
         }
