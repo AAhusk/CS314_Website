db2844a550c0571bb8f5075b68bd3789218d7e12
aahusk <54596652+aahusk@users.noreply.github.com>
2019-12-11 23:25:23 -0700

    Change layout of place information.

--- client/src/components/Application/Calculator/Calculator.js
+++ client/src/components/Application/Calculator/Calculator.js
@@ -145,7 +145,15 @@ export default class Calculator extends Component {
 						<h4>Place</h4>
 						<Row>
 							<Col m={6} lg={5}><b>Location</b></Col>
-							<Col m={6} lg={3}>{this.state.DBplace.name}, {this.state.DBplace.municipality}, {this.state.DBplace.country}</Col>
+							<Col m={6} lg={3}>{this.state.DBplace.name}</Col>
+						</Row>
+						<Row>
+							<Col m={6} lg={5}><b>Country</b></Col>
+							<Col m={6} lg={3}>{this.state.DBplace.country}</Col>
+						</Row>
+						<Row>
+							<Col m={6} lg={5}><b>Municipality</b></Col>
+							<Col m={6} lg={3}>{this.state.DBplace.municipality}</Col>
 						</Row>
 						<Row>
 							<Col m={6} lg={5}><b>Coordinates</b></Col>
